cf955ad17cb54de7ada50cb4b3bbd87919a247e3
Konstantin Simeonov <kon.simeonov@protonmail.com>
2023-04-20 16:05:12 +0300

    fix the watch

--- watch.sh
+++ watch.sh
@@ -1,11 +1,18 @@
+die() {
+    kill $(jobs -p)
+}
+
+trap 'die' SIGINT SIGTERM
+
 inotifywait \
     --monitor \
-    --format "%w%f"  \
-    --event modify,move,create,delete *.dot |  \
+    --format "%w%f" \
+    --event modify,move,create,delete *.dot | \
     while read f; do
-        echo "CHANGED %f"
-        dot -Tsvg "%f" > roadmap.svg
-        echo "Built svg"
+        echo "CHANGED $f"
+        out_file="${f/dot/svg}"
+        dot -Tsvg "$f" -o "$out_file"
+        echo "Built svg $out_file"
     done &
 
 live-server .
